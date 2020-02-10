dedfb77775f9edc8d6cb02504894fc8f12bde92c
Devin Checa <devincheca@gmail.com>
2020-02-09 20:32:45 -0800

    this should work

---
+++ centinela-adobe/build/precache-manifest.4ca6c4f555cb7c0954d28f2241e48911.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260c3db2c1102432e9dddcbccf204a09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "986472234bc77c748149",
+    "url": "/static/css/main.8905dfc1.chunk.css"
+  },
+  {
+    "revision": "29ba6a5161fc628e56be",
+    "url": "/static/js/2.8cef289f.chunk.js"
+  },
+  {
+    "revision": "986472234bc77c748149",
+    "url": "/static/js/main.54120f15.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
